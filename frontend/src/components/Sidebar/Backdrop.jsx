6f1761da805924fcdcc5ddd7467989eac61ff67a
Althaf Ariq M. G <71364028+althafariq@users.noreply.github.com>
2022-10-21 08:59:06 +0700

    Delete frontend directory

--- frontend/src/components/Sidebar/Backdrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Backdrop.scss";
-
-function Backdrop({ Sidebar, closeSidebar }) {
-  return <div className={Sidebar ? "backdrop backdrop-open" : "backdrop"} onClick={closeSidebar}></div>;
-}
-
-export default Backdrop;
